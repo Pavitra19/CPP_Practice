2c83a19d676b5610739f9472128fc5aad193319d
Pavitra19 <43502233+Pavitra19@users.noreply.github.com>
2019-05-04 23:16:10 -0400

    Create BasicDataTypes.cpp

---
+++ HackerRank/Easy/BasicDataTypes.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+int main() {
+
+int n;
+long x;
+char a;
+float u;
+double z;
+scanf("%d\n%ld\n%c\n%f\n%lf\n", &n, &x, &a, &u, &z);
+printf("%d\n%ld\n%c\n%f\n%lf\n", n,x,a,u,z);
+return 0;
+}
+
