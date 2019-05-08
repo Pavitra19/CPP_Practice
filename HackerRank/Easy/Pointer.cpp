34bf99e5042f1ed39102fca407ab530cfba88a15
Pavitra19 <43502233+Pavitra19@users.noreply.github.com>
2019-05-08 12:03:34 -0400

    Update Pointer.cpp

--- HackerRank/Easy/Pointer.cpp
+++ HackerRank/Easy/Pointer.cpp
@@ -1,8 +1,39 @@
+
+//With absolute function 
+
 #include <stdio.h>
 #include <stdlib.h>
 
-void update(int *a,int *b) {
+void update(int *a, int *b) {
 
+    int sum = *a + *b;
+    int abs_diff = abs(*a - *b);
+    *a = sum; 
+    *b = abs_diff; 
+}
+
+int main() {
+  int a, b;
+  int *pa = &a, *pb = &b;
+
+  scanf("%d %d", &a, &b);
+  update(pa, pb);
+  printf("%d\n%d", a, b);
+
+  return 0;
+}
+
+
+//Version 2
+
+#include <stdio.h>
+
+void update(int *a,int *b) {
+    int sum = *a + *b;
+    int abs_diff = *a - *b >= 0? *a - *b : -(*a - *b); 
+    *a = sum;
+    *b = abs_diff; 
+    
 }
 
 int main() {
@@ -11,7 +42,7 @@ int main() {
     
     scanf("%d %d", &a, &b);
     update(pa, pb);
-    printf("%d\n%d", a + b, abs(a-b));
+    printf("%d\n%d", a, b);
 
     return 0;
 }
