0599284c1b926fdd494d44d3acebdb0eefbd481a
Pavitra19 <43502233+Pavitra19@users.noreply.github.com>
2019-05-08 19:10:29 -0400

    Update Arrays.cpp

--- HackerRank/Easy/Arrays.cpp
+++ HackerRank/Easy/Arrays.cpp
@@ -11,7 +11,7 @@ int main() {
     int N, i=0;
     std::cin>> N;
     int *C = new int[N];
-    while(std::cin>>C[i++]);
+    while(std::cin>>C[i++]); //loop ends when cin reads EOF
     while(std::cout<<C[--N]<<' ' && N);
     delete [] C; //free the memory assigned to C since no longer needed 
     return 0;
